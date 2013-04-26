4dadb75663329cc9f9903d06b225c86963b01fb7
Erez Eskenazi <erezesk@gmail.com>
2013-04-26 08:51:19 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Keekit::Application.config.secret_token = 'e27e012b39bf126c168b687630459fdf7b72083dcb83ed4ad1eb5ad19b0c871744d25b4c474db757025cb2606dd827b781b3b4a989aff10fdd3b55a3a231eb52'
