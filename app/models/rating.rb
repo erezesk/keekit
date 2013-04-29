ea6373c23d2529e050664f3a9f95eb4439c3190e
Erez Eskenazi <erezesk@gmail.com>
2013-04-29 21:01:04 +0300

    annotated

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: ratings
+#
+#  id               :integer          not null, primary key
+#  value            :integer
+#  advertisement_id :integer
+#  user_id          :integer
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+
 class Rating < ActiveRecord::Base
   attr_accessible :advertisement_id, :user_id, :value, :created_at, :updated_at
 
--- test/unit/rating_test.rb
+++ test/unit/rating_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: ratings
+#
+#  id               :integer          not null, primary key
+#  value            :integer
+#  advertisement_id :integer
+#  user_id          :integer
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+
 require 'test_helper'
 
 class RatingTest < ActiveSupport::TestCase
