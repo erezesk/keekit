4579a44c01fd5f6d5dffb584dc70b948313da1c6
Erez Eskenazi <erezesk@gmail.com>
2013-06-03 13:43:34 +0300

    annotate

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,14 @@
 #  id                :integer          not null, primary key
 #  username          :string(255)
 #  email             :string(255)
-#  type              :string(255)
-#  birthday          :datetime
-#  country           :string(255)
+#  birthday          :date
+#  gender            :string(255)
 #  crypted_password  :string(255)
 #  password_salt     :string(255)
 #  persistence_token :string(255)
 #  created_at        :datetime         not null
 #  updated_at        :datetime         not null
+#  user_type         :string(255)
 #
 
 class User < ActiveRecord::Base
